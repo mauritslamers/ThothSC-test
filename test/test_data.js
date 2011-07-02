08f52bf639f39a4794ce901283bcc002403f9b22
Maurits Lamers <maurits@weidestraat.nl>
2011-07-02 20:45:42 +0200

    fixing schema and complete the models

--- test/test_data.js
+++ test/test_data.js
@@ -63,42 +63,63 @@ ThothSC.fetchResult = {
 schema:
 
                      ┌ ---------------------------------┐
-                    1m                                  1
-Student ∞m ---- ∞ Lesson 1m ---- 1m Course ∞m ---- ∞m Teacher
-   ∞                1                 1                 1m
+                    1                                   ∞m
+Student ∞ ---- ∞m Lesson 1m ---- 1m Course ∞m ---- ∞m Teacher
+   ∞                1                 ∞                 ∞m
    │                |                 |                 |
    |                1m                |                 |
-   └ -------- 1m Assignment  ∞m ------┘                 |
-                    ∞m                                  |
+   └ -------- 1m Assignment  1m ------┘                 |
+                    1m                                  |
                     └ ----------------------------------┘
 
+This situation is rather unlikely of course (a teacher and a lesson & a course and a lesson in a one-to-one), 
+but this is done just for testing purposes.
 */
 
 
 ThothSC.Student = SC.Record.extend({
+  primaryKey: 'id',
   firstname: SC.Record.attr(String),
   inbetween: SC.Record.attr(String),
   lastname: SC.Record.attr(String),
-  lessons: SC.Record.toMany('ThothSC.Lesson', { isMaster: true, oppositeProperty: 'students' }),
+  lessons: SC.Record.toMany('ThothSC.Lesson', { isMaster: false, oppositeProperty: 'students' }),
   assignments: SC.Record.toMany('ThothSC.Assignment', { isMaster: false, oppositeProperty: 'student'})
 });
 
 ThothSC.Course = SC.Record.extend({
+  primaryKey: 'id',
   name: SC.Record.attr(String),
   year: SC.Record.attr(String),
   assignments: SC.Record.toMany('ThothSC.Assignment', { isMaster: false, oppositeProperty: 'course'}),
   lesson: SC.Record.toOne('ThothSC.Lesson', { isMaster: true, oppositeProperty: 'course'}), // rather unlikely but 1-1 for the sake of testing
-  teachers: SC.Record.toMany('ThothSC.Teacher', { isMaster: true })
+  teachers: SC.Record.toMany('ThothSC.Teacher', { isMaster: true, oppositeProperty: 'courses' })
 });
 
 ThothSC.Teacher = SC.Record.extend({
-  
+  primaryKey: 'id',
+  firstname: SC.Record.attr(String),
+  inbetween: SC.Record.attr(String),
+  lastname: SC.Record.attr(String),
+  assignments: SC.Record.toMany('ThothSC.Assignment', {isMaster: true, oppositeProperty: 'teacher'}),
+  lessons: SC.Record.toMany('ThothSC.Lesson', { isMaster: true, oppositeProperty: 'teacher'}),
+  courses: SC.Record.toMany('ThothSC.Course', { isMaster: true, oppositeProperty: 'teachers'})
 });
 
 ThothSC.Lesson = SC.Record.extend({
-  
+  primaryKey: 'id',
+  moment: SC.Record.attr(String),
+  teacher: SC.Record.toOne('ThothSC.Teacher', { isMaster: false, oppositeProperty: 'lesson'}),
+  course: SC.Record.toOne('ThothSC.Course', { isMaster: true, oppositeProperty: 'lesson'}),
+  students: SC.Record.toMany('ThothSC.Student', { isMaster: true, oppositeProperty: 'lessons'}),
+  assignment: SC.Record.toOne('ThothSC.Assignment', { isMaster: false, oppositeProperty: 'lessons'})
 });
 
 ThothSC.Assignment = SC.Record.extend({
-  
+  primaryKey: 'id',
+  name: SC.Record.attr(String),
+  moment: SC.Record.attr(String),
+  student: SC.Record.toOne('ThothSC.Student', { isMaster: true, oppositeProperty: 'assignments'}),
+  course: SC.Record.toOne('ThothSC.Course', { isMaster: true, oppositeProperty: 'assignments' }),
+  lesson: SC.Record.toOne('ThothSC.Lesson', { isMaster: true, oppositeProperty: 'assignment'}),
+  teacher: SC.Record.toOne('ThothSC.Teacher', { isMaster: true, oppositeProperty: 'assignments'})
 });
